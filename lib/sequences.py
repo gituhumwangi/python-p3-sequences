e28e239b9ec951c58a68cd7351cbc04acc09ca3d
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-21 10:03:42 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,23 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        return[]
+    elif length == 1:
+        return[0]
+
+    
+    fibonacci_sequence = [0,1]
+    while len (fibonacci_sequence) < length :
+        next_number=fibonacci_sequence[-1] + fibonacci_sequence[-2] 
+        next_number.append(fibonacci_sequence)
+    print(fibonacci_sequence)
+    return fibonacci_sequence
+    
+    
+
+    
+
+    
+
+
