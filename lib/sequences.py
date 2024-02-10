52d3a3d8f1bf9bf6ca4ddece219dc28fba38df1d
dennis <dennis.mwangi@student.moringaschool.com>
2024-02-10 08:44:17 +0300

    Corrected code

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
+def print_fibonacci(num):
     pass
-    if length <= 0:
-        return[]
-    elif length == 1:
-        return[0]
+    if num <= 0:
+        print([])
+    elif num == 1:
+        print([0])
+    elif num == 2:
+        print([0,1])
 
     
     fibonacci_sequence = [0,1]
-    while len (fibonacci_sequence) < length :
+    while len (fibonacci_sequence) < num :
         next_number=fibonacci_sequence[-1] + fibonacci_sequence[-2] 
-        next_number.append(fibonacci_sequence)
-    print(fibonacci_sequence)
+        fibonacci_sequence.append(next_number)
+    # print(fibonacci_sequence)
     return fibonacci_sequence
     
     
-
     
+
+print(print_fibonacci(10))
+
 
     
 
